559b10574cc6c4734d8850e24a9cdfba9ea3c7df
Sami Hafid <sahafid@e1r10p13.1337.ma>
2022-02-25 14:06:31 +0100

    seperating styles and images in seperate files and fixing form

--- app.js
+++ app.js
@@ -5,78 +5,118 @@
 var card1 = document.querySelector(".card1");
 var card2 = document.querySelector(".card2");
 var card3 = document.querySelector(".card3");
+var card_container = document.querySelector(".card-container");
+var form_card = document.querySelector(".form-card");
+
 var image1 = document.querySelector(".image1");
 var image2 = document.querySelector(".image2");
 var image3 = document.querySelector(".image3");
+var image4 = document.querySelector(".image4");
+
+
 var text_cards1 = document.querySelector(".text-cards1");
 var text_cards2 = document.querySelector(".text-cards2");
 var text_cards3 = document.querySelector(".text-cards3");
 
 // creating new elements
 
-var subcard = document.createElement('div');
-subcard.classList.add('animations');
 
-document.querySelector(".card1").addEventListener('mouseover', function ()
+
+var textElement1 = document.createElement('p');
+var textElement2 = document.createElement('p');
+var textElement3 = document.createElement('p');
+var link = document.createElement('a');
+
+
+//subcard1.classList.add('animations');
+
+card1.addEventListener('mouseover', function ()
 {
-    card1.style.backgroundColor = "#F4D03F";
-    card1.appendChild(subcard);
+    card1.style.backgroundColor = "#e32727";
     image1.style.display = "none";
     text_cards1.style.display = "none";
+    card1.appendChild(textElement1);
+	textElement1.style.display = "grid";
+    textElement1.innerHTML = "<p><b>YOU WANNA JOIN US ? </p>JUST PRESS THE TOP BUTTON</b><img class='image4' src='assets/money2.svg'>";
+    textElement1.style.fontWeight = "300";
+    textElement1.style.fontSize = "20px";
+    textElement1.style.textAlign= "center";
+    textElement1.style.color= "white";
     
-    subcard.style.display = "block";
-    
-    // subcard.style.backgroundColor = "white";
-    // subcard.style.width = "100%";
-    // subcard.style.height = "100%";
-    // subcard.style.cursor= "pointer";
-    // subcard.style.borderRadius= "15px";
-    // subcard.style.transform = "translateY(-50px)";
-    // subcard.style.transform = "translateY(0px)";
 });
 
-
-document.querySelector(".card1").addEventListener('mouseout', function ()
+card1.addEventListener('mouseout', function ()
 {
     card1.style.backgroundColor = "white";
-    subcard.style.display = "none";
     image1.style.display = "inline";
     text_cards1.style.display = "block";
-
+	textElement1.style.display = "none";
 });
 
-// boxOne.classList.add('horizTranslate');
 
-document.querySelector(".card2").addEventListener('mouseover', function ()
+card2.addEventListener('mouseover', function ()
 {
-    card2.style.backgroundColor = "#F4D03F";
+    card2.style.backgroundColor = "#e32727";
     image2.style.display = "none";
     text_cards2.style.display = "none";
-    card2.appendChild(subcard);   
-    subcard.style.display = "block";
+    card2.appendChild(textElement2);
+	textElement2.style.display = "grid";
+    textElement2.innerHTML = "<p><b>YOU WANNA JOIN US ? </p>JUST PRESS THE TOP BUTTON</b><img class='image4' src='assets/money2.svg'>";
+    textElement2.style.fontWeight = "300";
+    textElement2.style.fontSize = "20px";
+    textElement2.style.textAlign= "center";
+    textElement2.style.color= "white";
 });
 
-document.querySelector(".card2").addEventListener('mouseout', function ()
+card2.addEventListener('mouseout', function ()
 {
     card2.style.backgroundColor = "white";
-    subcard.style.display = "none";
     image2.style.display = "inline";
     text_cards2.style.display = "block";
+    textElement2.style.display = "none";
 });
 
-document.querySelector(".card3").addEventListener('mouseover', function ()
+card3.addEventListener('mouseover', function ()
 {
-    card3.style.backgroundColor = "#F4D03F";
+    card3.style.backgroundColor = "#e32727";
     image3.style.display = "none";
     text_cards3.style.display = "none";
-    card3.appendChild(subcard);  
-    subcard.style.display = "block";
+    card3.appendChild(textElement3);
+	textElement3.style.display = "grid";
+    textElement3.innerHTML = "<p><b>YOU WANNA JOIN US ? </p>JUST PRESS THE TOP BUTTON</b><img class='image4' src='assets/money2.svg'>";
+    textElement3.style.fontWeight = "300";
+    textElement3.style.fontSize = "20px";
+    textElement3.style.textAlign= "center";
+    textElement3.style.color= "white";
+
 });
 
-document.querySelector(".card3").addEventListener('mouseout', function ()
+card3.addEventListener('mouseout', function ()
 {  
     card3.style.backgroundColor = "white";
-    subcard.style.display = "none";
     image3.style.display = "inline";
     text_cards3.style.display = "block";
+    textElement3.style.display = "none";
 });
+
+card_container.addEventListener('mouseover', function ()
+{
+    card_container.style.backgroundColor = "#e32727";
+});
+
+card_container.addEventListener('mouseout', function ()
+{  
+    card_container.style.backgroundColor = "white";
+});
+
+// form_card.addEventListener('mouseover', function ()
+// {
+//     form_card.style.backgroundColor = "#e32727";
+//     document.querySelector(".form-inputs").style.backgroundColor = "#e32727";
+// });
+
+// form_card.addEventListener('mouseout', function ()
+// {  
+//     form_card.style.backgroundColor = "white";
+//     document.querySelector(".form-inputs").style.backgroundColor = "white";
+// });
