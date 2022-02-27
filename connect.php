fc69963626a5f199fd19110e1d9ea1322f742e34
Sami Hafid <sahafid@e2r6p15.1337.ma>
2022-02-27 17:56:13 +0100

    adding php file

--- connect.php
+++ connect.php
@@ -1,8 +1,22 @@
 <?php
-    $Name = $_POST['NAME'];
+    $Name = $_POST['Name'];
     $Email = $_POST['Email'];
     $Password = $_POST['Password'];
     $Phone = $_POST['Phone'];
     $Company = $_POST['Company'];
 
+    $conn = new mysqli('local', 'root', '','100th');
+    if ($conn->connect_error)
+    {
+        die('Connection Failed : '.$conn->connect_error);
+    }
+    else
+    {
+        $stmt = $conn->prepare("insert into registration(Name, Email, Password, Phone, Company) values(?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssis", $Name, $Email, $Password, $Phone, $Company);
+        $stmt->execute();
+        echo "registration Successfully...";
+        $stmt->close();
+        $conn->close();
+    }
 ?>
